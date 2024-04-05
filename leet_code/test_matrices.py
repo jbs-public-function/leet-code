11204bc7ec8ba74375038f3c9e98885e1ed188b4
JB <54007352+jbs-public-function@users.noreply.github.com>
2024-04-05 16:18:05 -0500

    Clean up directory structure and centralize leet code tests based on type (trees, linked_lists, matrices, etc). Delete redundant files. Add rotate test under the test_matrices.py file. update .gitignore (#10)

---
+++ leet_code/test_matrices.py
@@ -0,0 +1,35 @@
+# python std library
+import unittest
+from collections import deque
+from typing import *
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        max_row_idx = n // 2
+        row_idx = 0
+        while row_idx < max_row_idx:
+            edge = n - row_idx - 1
+            for col_idx in range(row_idx, edge):
+                edge_mod = edge - col_idx * 1 + row_idx
+                top_left  = matrix[row_idx][col_idx]
+                top_right = matrix[col_idx][edge]
+                btm_right = matrix[edge][edge_mod]
+                btm_left  = matrix[edge_mod][row_idx]
+
+                matrix[row_idx][col_idx]  = btm_left
+                matrix[col_idx][edge]     = top_left
+                matrix[edge][edge_mod]    = top_right
+                matrix[edge_mod][row_idx] = btm_right
+            row_idx += 1
+
+class testMatrices(unittest.TestCase):
+    def test_case_rotate_in_memory_a(self):
+        matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+        expected_output = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+        Solution().rotate(matrix)
+        assert expected_output == matrix
---
+++ leet_code/test_trees.py
@@ -0,0 +1,56 @@
+# python std library
+import unittest
+from collections import deque
+from typing import *
+
+# internal
+from leet_code.data_structures import make_arbitrary_trees, TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        max_depth = 0
+        if not root:
+            return max_depth
+
+        q = deque([root])
+        child_q = deque([])
+
+        while q:
+            max_depth += 1
+            while q:
+                parent = q.pop()
+                if parent.left:
+                    child_q.append(parent.left)
+                if parent.right:
+                    child_q.append(parent.right)
+            tmp_q = q
+            q = child_q
+            child_q = tmp_q
+        return max_depth
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        data = []
+        def depth_first_search(root: Optional[TreeNode]):
+            if not root:
+                return
+            depth_first_search(root.left)
+            data.append(root.val)
+            depth_first_search(root.right)
+        depth_first_search(root)
+        return data
+
+
+class TestTrees(unittest.TestCase):
+    def test_case_max_depth(self):
+        test_case = [3,9,20,None,None,15,7]
+        root = make_arbitrary_trees(test_case)
+        depth = Solution().maxDepth(root)
+
+        assert depth == 3
+
+    def test_case_inorder_tree_traversal(self):
+        test_case = [1, None, 2, 3]
+        root = make_arbitrary_trees(test_case)
+        soln = Solution().inorderTraversal(root)
+        assert soln == [1, 3, 2]
